9be74a6a5fb19d04d890bec9b8c9e16eb544c1eb
Joshua Martinez <joshkmartinez@gmail.com>
2019-05-26 15:34:56 -0700

    add resume page

--- components/layout/top.js
+++ components/layout/top.js
@@ -1,23 +1,18 @@
 import Link from '../Link'
-import { Flex, Box } from 'rebass'
-const Holder = ({ children }) => (
-  <div>
-    {children}
-    <style jsx>{`
-      text-align: center;
-      font-family: Roboto;
-    `}</style>
-  </div>
-)
+import { Flex, Box, Text } from 'rebass'
+import Holder from './CenterHolder'
 const Top = () => (
   <Holder>
     <Flex>
-      <Box p={3} width={1 / 2} /*g="whitesmoke"*/>
-        Joshua Martinez
+      <Box p={3} width={1 / 2} /*bg="whitesmoke"*/>
+        <Link /*this will redirect to home*/>
+          <Text fontWeight={900}>Joshua Martinez</Text>
+        </Link>
       </Box>
       <Box p={3} width={1 / 2} /*bg="grey"*/>
         <Link href="/projects">Projects</Link>
         <Link href="/about">About</Link>
+        <Link href="/resume">Résumé</Link>
       </Box>
     </Flex>
   </Holder>
