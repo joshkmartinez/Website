6677cba9b5d01f3cd16fcdaa63a9019abe9b4de4
Joshua Martinez <joshkmartinez@gmail.com>
2020-01-20 10:05:33 -0800

    add terminal

---
+++ pages/terminal.js
@@ -0,0 +1,18 @@
+const terminal = () => (
+  <div
+    style={{
+      margin: 0,
+      height: '100vh'
+    }}
+  >
+    <iframe
+      style={{
+        border: 'none',
+        width: '100%',
+        height: '100%'
+      }}
+      src="https://terminal.joshkmartinez.repl.run"
+    ></iframe>
+  </div>
+)
+export default terminal
